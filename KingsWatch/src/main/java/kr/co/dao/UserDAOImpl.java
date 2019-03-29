f974a9941e71341a4f2c10a78e9e569f13a1fe41
ggoomter <ggoomter@gmail.com>
2019-03-29 17:47:07 +0900

    회원등록 첫페이지 뜸

---
+++ KingsWatch/src/main/java/kr/co/dao/UserDAOImpl.java
@@ -0,0 +1,56 @@
+package kr.co.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.domain.UserDTO;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private final String NS="mapper.user";
+	
+
+	@Override
+	public void insert(UserDTO dto) {
+		sqlSession.insert(NS+".insert", dto);
+	}
+
+	@Override
+	public List<UserDTO> select() {
+		return sqlSession.selectList(NS+".list");
+	}
+
+	@Override
+	public UserDTO selectById(int id) {
+		return sqlSession.selectOne(NS+".selectById", id);	}
+
+	@Override
+	public UserDTO updateui(int id) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne(NS+".updateui", id);
+	}
+
+	@Override
+	public void update(UserDTO dto) {
+		// TODO Auto-generated method stub
+		sqlSession.update(NS+".update", dto);
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		sqlSession.insert(NS+".delete", id);
+	}
+
+	
+	
+
+}
